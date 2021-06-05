d49175be8fc8b75728006bdf6a4027e3c3638928
LuanLion <nvvluan2019@gmail.com>
2021-06-05 12:33:08 +0700

    first commit

---
+++ NetBeansProjects/HelloFX/src/newpackage/Hellofx.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackage;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Admin
+ */
+public class Hellofx extends Application{
+
+    @Override
+    public void start(Stage arg0) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
